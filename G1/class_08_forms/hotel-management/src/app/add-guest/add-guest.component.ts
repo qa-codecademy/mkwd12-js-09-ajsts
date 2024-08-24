4d12aea3c4096d02d61b7548a1c1488226067f18
Ivo Kostovski <ivo.kostovski@gmail.com>
2024-08-24 11:31:13 +0200

    add validations to form

--- G1/class_08_forms/hotel-management/src/app/add-guest/add-guest.component.ts
+++ G1/class_08_forms/hotel-management/src/app/add-guest/add-guest.component.ts
@@ -1,12 +1,157 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  MAT_DATE_LOCALE,
+  provideNativeDateAdapter,
+} from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-add-guest',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatLabel,
+    MatInputModule,
+    MatDatepickerModule,
+  ],
+  providers: [
+    provideNativeDateAdapter(),
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+  ],
   templateUrl: './add-guest.component.html',
-  styleUrl: './add-guest.component.css'
+  styleUrl: './add-guest.component.css',
 })
-export class AddGuestComponent {
+export class AddGuestComponent implements OnInit {
+  guestForm = new FormGroup({
+    firstName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]),
+    lastName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(50),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    dateOfBirth: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required]),
+    address: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(100),
+    ]),
+    passportNumber: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(20),
+    ]),
+  });
 
+  get firstNameErrors(): string {
+    // first if as example of handling proper state for validation:
+    // form control MUST be invalid AND (either touched OR dirty)
+    if (
+      this.guestForm.get('firstName')?.invalid &&
+      (this.guestForm.get('firstName')?.touched ||
+        this.guestForm.get('firstName')?.dirty)
+    ) {
+      if (this.guestForm.get('firstName')?.errors?.['required']) {
+        return 'First Name is required';
+      }
+
+      if (this.guestForm.get('firstName')?.errors?.['minlength']) {
+        return 'First Name must have at least 3 characters';
+      }
+
+      if (this.guestForm.get('firstName')?.errors?.['maxlength']) {
+        return 'First Name must be at most 20 characters long';
+      }
+    }
+
+    return '';
+  }
+
+  get lastNameErrors(): string {
+    if (this.guestForm.get('lastName')?.errors?.['required']) {
+      return 'Last Name is required';
+    }
+
+    if (this.guestForm.get('lastName')?.errors?.['minlength']) {
+      return 'Last Name must have at least 3 characters';
+    }
+
+    if (this.guestForm.get('lastName')?.errors?.['maxlength']) {
+      return 'Last Name must be at most 50 characters long';
+    }
+
+    return '';
+  }
+
+  get emailErrors(): string {
+    if (this.guestForm.get('email')?.errors?.['required']) {
+      return 'Email is required';
+    }
+
+    if (this.guestForm.get('email')?.errors?.['email']) {
+      return 'Email must be in valid format';
+    }
+
+    return '';
+  }
+
+  get dateOfBirthErrors(): string {
+    if (this.guestForm.get('dateOfBirth')?.errors?.['required']) {
+      return 'Date of birth is required';
+    }
+
+    return '';
+  }
+
+  get phoneNumberErrors(): string {
+    if (this.guestForm.get('phone')?.errors?.['required']) {
+      return 'Phone number is required';
+    }
+
+    return '';
+  }
+
+  get addressErrors(): string {
+    if (this.guestForm.get('address')?.errors?.['required']) {
+      return 'Address is required';
+    }
+
+    if (this.guestForm.get('address')?.errors?.['maxlength']) {
+      return 'Address must be at most 100 characters long';
+    }
+
+    return '';
+  }
+
+  get passportNumberErrors(): string {
+    if (this.guestForm.get('passportNumber')?.errors?.['required']) {
+      return 'Passport number is required';
+    }
+
+    if (this.guestForm.get('passportNumber')?.errors?.['maxlength']) {
+      return 'Passport number must be at most 20 characters long';
+    }
+
+    return '';
+  }
+
+  ngOnInit(): void {
+    this.guestForm.valueChanges.subscribe((value) => console.log(value));
+  }
+
+  onSubmit() {
+    console.log('Submitting...', this.guestForm);
+  }
 }
--- G1/class_08_forms/hotel-management/src/app/select-guest/select-guest.component.ts
+++ G1/class_08_forms/hotel-management/src/app/select-guest/select-guest.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-select-guest',
   standalone: true,
   imports: [],
   templateUrl: './select-guest.component.html',
-  styleUrl: './select-guest.component.css'
+  styleUrl: './select-guest.component.css',
 })
 export class SelectGuestComponent {
-
+  
 }
