937b94e0f08fa919570e5b25b501153783e57c19
Ivo Kostovski <ivo.kostovski@gmail.com>
2024-08-26 18:28:01 +0200

    Class 9

---
+++ G1/class_08_forms/hotel-management/src/services/bookings.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CreateBooking } from '../types/booking.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookingsService {
+  bookingsPath = `http://localhost:3000/api/bookings`;
+
+  constructor(private readonly http: HttpClient) {}
+
+  createBooking(booking: CreateBooking) {
+    return this.http.post<any>(this.bookingsPath, booking);
+  }
+}
