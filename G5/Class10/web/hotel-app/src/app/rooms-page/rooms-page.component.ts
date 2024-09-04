6aeb0c73f6b8da09dc7e6c21e94890069bbcda90
anetastankovska <anetastankovskaane@gmail.com>
2024-09-04 20:55:50 +0200

    finish class 11

--- G5/Class10/web/hotel-app/src/app/rooms-page/rooms-page.component.ts
+++ G5/Class10/web/hotel-app/src/app/rooms-page/rooms-page.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { MatSortModule, Sort } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { RoomsStore } from '../../store/rooms.store';
+import { Subscription } from 'rxjs';
+import { RoomService } from '../../services/room.service';
+import { SearchRoomQuery } from '../../types/search-room-query.interface';
 
 @Component({
   selector: 'app-rooms-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule,
+    MatSortModule,
+    CommonModule,
+    MatPaginatorModule,
+    RouterLink,
+  ],
   templateUrl: './rooms-page.component.html',
   styleUrl: './rooms-page.component.css'
 })
 export class RoomsPageComponent {
+  displayedColumns: string[] = [
+    'name',
+    'type',
+    'pricePerNight',
+    // 'beds',
+    // 'extraBeds',
+    // 'baths',
+    // 'guestCapacity',
+    // 'view',
+    // 'parking',
+    // 'isPetFriendly',
+    // 'board',
+    // 'hasAirConditioning',
+  ]
 
+  roomsStore = inject(RoomsStore);
+  subscription: Subscription = new Subscription();
+
+  constructor(
+    private roomService: RoomService
+  ) {
+    effect(() => {
+      this.getRooms(this.roomsStore.searchParams());
+    })
+  }
+
+  getRooms(searchParams: SearchRoomQuery = {}) {
+    this.subscription = this.roomService
+      .getRooms(searchParams)
+      .subscribe((response) => {
+        this.roomsStore.setTotal(response.total);
+        this.roomsStore.setRooms(response.payload);
+        this.roomsStore.setLoading(false);
+      });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
